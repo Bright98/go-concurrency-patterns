41c5fe7f428236f80bbf3518248a9a2c8bdb4362
bright98 <www.hhsshhss@gmail.com>
2023-11-13 18:13:49 +0330

    add: fan out in

---
+++ fan-out-in/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func doTask(id int, tasks []int, out chan<- string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for task := range tasks {
+		result := fmt.Sprintf("task %d is doing by worker %d", task, id)
+		out <- result
+	}
+}
+
+func main() {
+	tasks := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+	workers := 3
+	results := make(chan string, len(tasks))
+	var wg sync.WaitGroup
+
+	//Fan Out: start multiple workers
+	for i := 0; i < workers; i++ {
+		wg.Add(1)
+		go doTask(i, tasks, results, &wg)
+	}
+
+	//Fan In: collect results and close it
+	go func() {
+		wg.Wait()
+		close(results)
+	}()
+
+	for result := range results {
+		fmt.Println("Result:", result)
+	}
+}
