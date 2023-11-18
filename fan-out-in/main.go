236e0bcb5aea05b502144fb6a4d2bee71e37f871
bright98 <www.hhsshhss@gmail.com>
2023-11-18 14:31:42 +0330

    fix: fan out in example

--- fan-out-in/main.go
+++ fan-out-in/main.go
@@ -5,24 +5,20 @@ import (
 	"sync"
 )
 
-func doTask(id int, tasks []int, out chan<- string, wg *sync.WaitGroup) {
+func doTask(task int, out chan<- string, wg *sync.WaitGroup) {
 	defer wg.Done()
-	for task := range tasks {
-		result := fmt.Sprintf("task %d is doing by worker %d", task, id)
-		out <- result
-	}
+	result := fmt.Sprintf("task %d done", task)
+	out <- result
 }
-
 func main() {
-	tasks := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
-	workers := 3
+	tasks := []int{10, 11, 50, 30, 54, 60, 90, 70}
 	results := make(chan string, len(tasks))
 	var wg sync.WaitGroup
 
 	//Fan Out: start multiple workers
-	for i := 0; i < workers; i++ {
+	for _, task := range tasks {
 		wg.Add(1)
-		go doTask(i, tasks, results, &wg)
+		go doTask(task, results, &wg)
 	}
 
 	//Fan In: collect results and close it
